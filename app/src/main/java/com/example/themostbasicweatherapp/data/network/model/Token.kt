d223e7d392ebe7dc544654cba6ea76db3b9b3cad
mNwoK <man.nikeman.21@gmail.com>
2024-12-27 13:43:35 +0300

    gross

--- app/src/main/java/com/example/themostbasicweatherapp/data/network/model/Token.kt
+++ app/src/main/java/com/example/themostbasicweatherapp/data/network/model/Token.kt
@@ -1,5 +1,5 @@
 package com.example.themostbasicweatherapp.data.network.model
 
 object Token {
-    const val TOKEN = "6e6412f2a1ad47bda2c121751242412"
+    val TOKEN = "6e6412f2a1ad47bda2c121751242412"
 }
